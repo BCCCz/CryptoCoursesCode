0b0cbb57268d5b5797b350610e37a2b41eda7277
BC <455288561@qq.com>
2020-11-28 14:36:25 +0800

    ElGamal
    ElGamal

--- 信息安全密码实验/Fourth/ElGamal.py
+++ 信息安全密码实验/Fourth/ElGamal.py
@@ -3,8 +3,23 @@ from gmpy2 import *
 import math
 import random
 
+def multimod(a,k,n):    #快速幂取模
+    ans=1
+    while(k!=0):
+        if k%2:         #奇数
+            ans=(ans%n)*(a%n)%n
+        a=(a%n)*(a%n)%n
+        k=k//2          #整除2
+    return ans
+
+def yg(n):		# 这样默认求最小原根
+    k=(n-1)//2
+    for i in range(2,n-1):
+        if multimod(i,k,n)!=1:
+            return i
+
 p = 2579
-g = 2
+g = yg(p)
 a = 765
 #明文
 m = 1299
