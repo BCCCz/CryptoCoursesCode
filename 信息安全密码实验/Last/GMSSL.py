ce33e0d3978567b39248d914c4d70546a48b7e3e
BC <455288561@qq.com>
2020-12-19 20:26:26 +0800

    SM2
    SM2

---
+++ 信息安全密码实验/Last/GMSSL.py
@@ -0,0 +1,378 @@
+'''
+M2 验收方法
+1.椭圆曲线参数，可以用文档例题中的，也可以直接用miracl库里封装的。
+2.Hash函数，可以用miracl库里其他Hash函数替代，只要格式合适即可；也可以自己编写SM3。
+3.验证时，会提供十个文件，每个包含一段或长或短的英文段落，作为明文。
+4.运行后屏幕显示必要的椭圆曲线参数，以及明文、对应密文、解密后的明文。
+'''
+
+from random import choice
+import binascii
+from random import choice
+from math import ceil
+# 选择素域，设置椭圆曲线参数
+
+#func
+xor = lambda a, b:list(map(lambda x, y: x ^ y, a, b))
+rotl = lambda x, n:((x << n) & 0xffffffff) | ((x >> (32 - n)) & 0xffffffff)
+get_uint32_be = lambda key_data:((key_data[0] << 24) | (key_data[1] << 16) | (key_data[2] << 8) | (key_data[3]))
+put_uint32_be = lambda n:[((n>>24)&0xff), ((n>>16)&0xff), ((n>>8)&0xff), ((n)&0xff)]
+padding = lambda data, block=16: data + [(16 - len(data) % block)for _ in range(16 - len(data) % block)]
+unpadding = lambda data: data[:-data[-1]]
+list_to_bytes = lambda data: b''.join([bytes((i,)) for i in data])
+bytes_to_list = lambda data: [i for i in data]
+random_hex = lambda x: ''.join([choice('0123456789abcdef') for _ in range(x)])
+
+
+default_ecc_table = {
+    'n': 'FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123',
+    'p': 'FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFF',
+    'g': '32c4ae2c1f1981195f9904466a39c9948fe30bbff2660be1715a4589334c74c7'
+         'bc3736a2f4f6779c59bdcee36b692153d0a9877cc62a474002df32e52139f0a0',
+    'a': 'FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFC',
+    'b': '28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93',
+}
+
+IV = [
+    1937774191, 1226093241, 388252375, 3666478592,
+    2842636476, 372324522, 3817729613, 2969243214,
+]
+
+T_j = [
+    2043430169, 2043430169, 2043430169, 2043430169, 2043430169, 2043430169,
+    2043430169, 2043430169, 2043430169, 2043430169, 2043430169, 2043430169,
+    2043430169, 2043430169, 2043430169, 2043430169, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042
+]
+
+
+#SM3杂凑函数
+def sm3_kdf(z, klen): # z为16进制表示的比特串（str），klen为密钥长度（单位byte）
+    klen = int(klen)
+    ct = 0x00000001
+    rcnt = ceil(klen/32)
+    zin = [i for i in bytes.fromhex(z.decode('utf8'))]
+    ha = ""
+    for i in range(rcnt):
+        msg = zin  + [i for i in binascii.a2b_hex(('%08x' % ct).encode('utf8'))]
+        ha = ha + sm3_hash(msg)
+        ct += 1
+    return ha[0: klen * 2]
+
+def sm3_cf(v_i, b_i):
+    w = []
+    for i in range(16):
+        weight = 0x1000000
+        data = 0
+        for k in range(i*4,(i+1)*4):
+            data = data + b_i[k]*weight
+            weight = int(weight/0x100)
+        w.append(data)
+
+    for j in range(16, 68):
+        w.append(0)
+        w[j] = sm3_p_1(w[j-16] ^ w[j-9] ^ (rotl(w[j-3], 15 % 32))) ^ (rotl(w[j-13], 7 % 32)) ^ w[j-6]
+        str1 = "%08x" % w[j]
+    w_1 = []
+    for j in range(0, 64):
+        w_1.append(0)
+        w_1[j] = w[j] ^ w[j+4]
+        str1 = "%08x" % w_1[j]
+
+    a, b, c, d, e, f, g, h = v_i
+
+    for j in range(0, 64):
+        ss_1 = rotl(
+            ((rotl(a, 12 % 32)) +
+            e +
+            (rotl(T_j[j], j % 32))) & 0xffffffff, 7 % 32
+        )
+        ss_2 = ss_1 ^ (rotl(a, 12 % 32))
+        tt_1 = (sm3_ff_j(a, b, c, j) + d + ss_2 + w_1[j]) & 0xffffffff
+        tt_2 = (sm3_gg_j(e, f, g, j) + h + ss_1 + w[j]) & 0xffffffff
+        d = c
+        c = rotl(b, 9 % 32)
+        b = a
+        a = tt_1
+        h = g
+        g = rotl(f, 19 % 32)
+        f = e
+        e = sm3_p_0(tt_2)
+
+        a, b, c, d, e, f, g, h = map(
+            lambda x:x & 0xFFFFFFFF ,[a, b, c, d, e, f, g, h])
+
+    v_j = [a, b, c, d, e, f, g, h]
+    return [v_j[i] ^ v_i[i] for i in range(8)]
+
+def sm3_p_0(x):
+    return x ^ (rotl(x, 9 % 32)) ^ (rotl(x, 17 % 32))
+    
+def sm3_p_1(x):
+    return x ^ (rotl(x, 15 % 32)) ^ (rotl(x, 23 % 32))
+
+def sm3_hash(msg):
+    # print(msg)
+    len1 = len(msg)
+    reserve1 = len1 % 64
+    msg.append(0x80)
+    reserve1 = reserve1 + 1
+    # 56-64, add 64 byte
+    range_end = 56
+    if reserve1 > range_end:
+        range_end = range_end + 64
+
+    for i in range(reserve1, range_end):
+        msg.append(0x00)
+
+    bit_length = (len1) * 8
+    bit_length_str = [bit_length % 0x100]
+    for i in range(7):
+        bit_length = int(bit_length / 0x100)
+        bit_length_str.append(bit_length % 0x100)
+    for i in range(8):
+        msg.append(bit_length_str[7-i])
+
+    group_count = round(len(msg) / 64)
+
+    B = []
+    for i in range(0, group_count):
+        B.append(msg[i*64:(i+1)*64])
+
+    V = []
+    V.append(IV)
+    for i in range(0, group_count):
+        V.append(sm3_cf(V[i], B[i]))
+
+    y = V[i+1]
+    result = ""
+    for i in y:
+        result = '%s%08x' % (result, i)
+    return result
+
+def sm3_ff_j(x, y, z, j):
+    if 0 <= j and j < 16:
+        ret = x ^ y ^ z
+    elif 16 <= j and j < 64:
+        ret = (x & y) | (x & z) | (y & z)
+    return ret
+
+def sm3_gg_j(x, y, z, j):
+    if 0 <= j and j < 16:
+        ret = x ^ y ^ z
+    elif 16 <= j and j < 64:
+        #ret = (X | Y) & ((2 ** 32 - 1 - X) | Z)
+        ret = (x & y) | ((~ x) & z)
+    return ret
+
+class CryptSM2(object):
+
+    def __init__(self, private_key, public_key, ecc_table=default_ecc_table, mode=0):
+        """
+        mode: 0-C1C2C3, 1-C1C3C2 (default is 1)
+        """
+        self.private_key = private_key
+        self.public_key = public_key
+        self.para_len = len(ecc_table['n'])
+        self.ecc_a3 = (
+            int(ecc_table['a'], base=16) + 3) % int(ecc_table['p'], base=16)
+        self.ecc_table = ecc_table
+        assert mode in (0, 1), 'mode must be one of (0, 1)'
+        self.mode = mode
+
+    def _kg(self, k, Point):  # kP运算
+        Point = '%s%s' % (Point, '1')
+        mask_str = '8'
+        for i in range(self.para_len - 1):
+            mask_str += '0'
+        mask = int(mask_str, 16)
+        Temp = Point
+        flag = False
+        for n in range(self.para_len * 4):
+            if (flag):
+                Temp = self._double_point(Temp)
+            if (k & mask) != 0:
+                if (flag):
+                    Temp = self._add_point(Temp, Point)
+                else:
+                    flag = True
+                    Temp = Point
+            k = k << 1
+        return self._convert_jacb_to_nor(Temp)
+
+    def _double_point(self, Point):  # 倍点
+        l = len(Point)
+        len_2 = 2 * self.para_len
+        if l < self.para_len * 2:
+            return None
+        else:
+            x1 = int(Point[0:self.para_len], 16)
+            y1 = int(Point[self.para_len:len_2], 16)
+            if l == len_2:
+                z1 = 1
+            else:
+                z1 = int(Point[len_2:], 16)
+
+            T6 = (z1 * z1) % int(self.ecc_table['p'], base=16)
+            T2 = (y1 * y1) % int(self.ecc_table['p'], base=16)
+            T3 = (x1 + T6) % int(self.ecc_table['p'], base=16)
+            T4 = (x1 - T6) % int(self.ecc_table['p'], base=16)
+            T1 = (T3 * T4) % int(self.ecc_table['p'], base=16)
+            T3 = (y1 * z1) % int(self.ecc_table['p'], base=16)
+            T4 = (T2 * 8) % int(self.ecc_table['p'], base=16)
+            T5 = (x1 * T4) % int(self.ecc_table['p'], base=16)
+            T1 = (T1 * 3) % int(self.ecc_table['p'], base=16)
+            T6 = (T6 * T6) % int(self.ecc_table['p'], base=16)
+            T6 = (self.ecc_a3 * T6) % int(self.ecc_table['p'], base=16)
+            T1 = (T1 + T6) % int(self.ecc_table['p'], base=16)
+            z3 = (T3 + T3) % int(self.ecc_table['p'], base=16)
+            T3 = (T1 * T1) % int(self.ecc_table['p'], base=16)
+            T2 = (T2 * T4) % int(self.ecc_table['p'], base=16)
+            x3 = (T3 - T5) % int(self.ecc_table['p'], base=16)
+
+            if (T5 % 2) == 1:
+                T4 = (T5 + ((T5 + int(self.ecc_table['p'], base=16)) >> 1) - T3) % int(
+                    self.ecc_table['p'], base=16)
+            else:
+                T4 = (T5 + (T5 >> 1) - T3) % int(self.ecc_table['p'], base=16)
+
+            T1 = (T1 * T4) % int(self.ecc_table['p'], base=16)
+            y3 = (T1 - T2) % int(self.ecc_table['p'], base=16)
+
+            form = '%%0%dx' % self.para_len
+            form = form * 3
+            return form % (x3, y3, z3)
+
+    def _add_point(self, P1, P2):  # 点加函数，P2点为仿射坐标即z=1，P1为Jacobian加重射影坐标
+        len_2 = 2 * self.para_len
+        l1 = len(P1)
+        l2 = len(P2)
+        if (l1 < len_2) or (l2 < len_2):
+            return None
+        else:
+            X1 = int(P1[0:self.para_len], 16)
+            Y1 = int(P1[self.para_len:len_2], 16)
+            if (l1 == len_2):
+                Z1 = 1
+            else:
+                Z1 = int(P1[len_2:], 16)
+            x2 = int(P2[0:self.para_len], 16)
+            y2 = int(P2[self.para_len:len_2], 16)
+
+            T1 = (Z1 * Z1) % int(self.ecc_table['p'], base=16)
+            T2 = (y2 * Z1) % int(self.ecc_table['p'], base=16)
+            T3 = (x2 * T1) % int(self.ecc_table['p'], base=16)
+            T1 = (T1 * T2) % int(self.ecc_table['p'], base=16)
+            T2 = (T3 - X1) % int(self.ecc_table['p'], base=16)
+            T3 = (T3 + X1) % int(self.ecc_table['p'], base=16)
+            T4 = (T2 * T2) % int(self.ecc_table['p'], base=16)
+            T1 = (T1 - Y1) % int(self.ecc_table['p'], base=16)
+            Z3 = (Z1 * T2) % int(self.ecc_table['p'], base=16)
+            T2 = (T2 * T4) % int(self.ecc_table['p'], base=16)
+            T3 = (T3 * T4) % int(self.ecc_table['p'], base=16)
+            T5 = (T1 * T1) % int(self.ecc_table['p'], base=16)
+            T4 = (X1 * T4) % int(self.ecc_table['p'], base=16)
+            X3 = (T5 - T3) % int(self.ecc_table['p'], base=16)
+            T2 = (Y1 * T2) % int(self.ecc_table['p'], base=16)
+            T3 = (T4 - X3) % int(self.ecc_table['p'], base=16)
+            T1 = (T1 * T3) % int(self.ecc_table['p'], base=16)
+            Y3 = (T1 - T2) % int(self.ecc_table['p'], base=16)
+
+            form = '%%0%dx' % self.para_len
+            form = form * 3
+            return form % (X3, Y3, Z3)
+
+    def _convert_jacb_to_nor(self, Point):  # Jacobian加重射影坐标转换成仿射坐标
+        len_2 = 2 * self.para_len
+        x = int(Point[0:self.para_len], 16)
+        y = int(Point[self.para_len:len_2], 16)
+        z = int(Point[len_2:], 16)
+        z_inv = pow(
+            z, int(self.ecc_table['p'], base=16) - 2, int(self.ecc_table['p'], base=16))
+        z_invSquar = (z_inv * z_inv) % int(self.ecc_table['p'], base=16)
+        z_invQube = (z_invSquar * z_inv) % int(self.ecc_table['p'], base=16)
+        x_new = (x * z_invSquar) % int(self.ecc_table['p'], base=16)
+        y_new = (y * z_invQube) % int(self.ecc_table['p'], base=16)
+        z_new = (z * z_inv) % int(self.ecc_table['p'], base=16)
+        if z_new == 1:
+            form = '%%0%dx' % self.para_len
+            form = form * 2
+            return form % (x_new, y_new)
+        else:
+            return None
+
+    def encrypt(self, data):
+        # 加密函数，data消息(bytes)
+        msg = data.hex()  # 消息转化为16进制字符串
+        k = random_hex(self.para_len) #用随机数发生器产生随机数𝒌 ∈ [𝟏,𝒏 − 1]
+        C1 = self._kg(int(k, 16), self.ecc_table['g']) #计算C1
+
+        xy = self._kg(int(k, 16), self.public_key)
+        print(xy)
+        #计算椭圆曲线点[𝒌]𝑷𝑩 = (𝒙𝟐, 𝒚𝟐)
+        x2 = xy[0:self.para_len]
+        y2 = xy[self.para_len:2*self.para_len]
+        ml = len(msg)
+
+        #计算𝒕 = 𝑲𝑫𝑭(𝒙𝟐 ∥ 𝒚𝟐, 𝒌𝒍𝒆𝒏)
+        t = sm3_kdf(xy.encode('utf8'), ml/2)
+        if int(t, 16) == 0:    #若𝒕为全0比特串，则返回A1
+            return None
+        else:
+            form = '%%0%dx' % ml
+            #C2 = 𝑴 ⊕ 𝒕；
+            C2 = form % (int(msg, 16) ^ int(t, 16)) 
+            #𝑪𝟑 = 𝑯𝒂𝒔𝒉(𝒙𝟐 ∥ 𝑴 ∥ 𝒚𝟐)
+            C3 = sm3_hash([
+                i for i in bytes.fromhex('%s%s%s' % (x2, msg, y2))
+            ])
+            #返回密文
+            print(bytes.fromhex('%s%s%s' % (C1, C2, C3)))
+            return bytes.fromhex('%s%s%s' % (C1, C2, C3))
+ 
+    def decrypt(self, data):
+        # 解密函数，data密文（bytes）
+        data = data.hex()
+        len_2 = 2 * self.para_len
+        len_3 = len_2 + 64
+        C1 = data[0:len_2]
+
+        if self.mode:
+            C3 = data[len_2:len_3]
+            C2 = data[len_3:]
+        else:
+            C2 = data[len_2:-64]
+            C3 = data[-64:]
+
+        xy = self._kg(int(self.private_key, 16), C1)
+        # print('xy = %s' % xy)
+        x2 = xy[0:self.para_len]
+        y2 = xy[self.para_len:len_2]
+        cl = len(C2)
+        t = sm3_kdf(xy.encode('utf8'), cl/2)
+        if int(t, 16) == 0:
+            return None
+        else:
+            form = '%%0%dx' % cl
+            M = form % (int(C2, 16) ^ int(t, 16))
+            u = sm3_hash([
+                i for i in bytes.fromhex('%s%s%s' % (x2, M, y2))
+            ])
+            print(bytes.fromhex(M))
+
+private_key = '00B9AB0B828FF68872F21A837FC303668428DEA11DCD1B24429D0C99E24EED83D5'
+public_key = 'B9C9A6E04E9C91F7BA880429273747D7EF5DDEB0BB2FF6317EB00BEF331A83081A6994B8993F3F5D6EADDDB81872266C87C018FB4162F5AF347B483E24620207'
+sm2_crypt = CryptSM2(public_key=public_key, private_key=private_key)
+data = b"It has done me good, because of the color of the wheat fields. Go and look again at the roses. You will understand now that yours is unique in all the world. "
+
+
+enc_data = sm2_crypt.encrypt(data)
+print(default_ecc_table)
+dec_data =sm2_crypt.decrypt(enc_data)
